f91f871e5f1eb6ca13533a15e10227661113deb9
abith07 <163716690+abith07@users.noreply.github.com>
2024-08-06 18:20:10 +0530

    Time: 2 ms (13.22%), Space: 41.9 MB (17.16%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String a[]=s.split("\\s+");
+       
+        return a[a.length-1].length();
+
+
+       
+            
+}
+          
+       
+        
+        
+    }
