9b163a1b55127b4942b483b0afbbadbaf112b30e
abith07 <163716690+abith07@users.noreply.github.com>
2024-08-06 18:19:28 +0530

    Time: 3 ms (52.03%), Space: 45.8 MB (26.8%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>max){
+                return false;
+            }
+            if(nums[i]+i>max){
+                max=nums[i]+i;
+
+            }
+           
+            }
+            return true;
+        }
+        
+
+        
+    }
