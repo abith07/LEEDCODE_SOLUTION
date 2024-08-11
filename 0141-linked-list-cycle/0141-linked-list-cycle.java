ccbb62e0456588498ecd644fc775bc3d513f83fe
abith07 <163716690+abith07@users.noreply.github.com>
2024-08-11 11:59:00 +0530

    Time: 0 ms (100%), Space: 44.1 MB (86.46%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,14 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        List<ListNode>al=new ArrayList<>();
-        while(head!=null){
-            if(al.contains(head)){
-                return true;
-            }
-            al.add(head);
-            head=head.next;
+       ListNode fast=head;
+       ListNode slow=head;
+       while(fast!=null&&fast.next!=null){
+        fast=fast.next.next;
+        slow=slow.next;
+        if(fast==slow){
+            return true;
         }
+       }
         return false;
         
     }
